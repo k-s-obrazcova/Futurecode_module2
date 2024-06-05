d86738cdb15ad9bfcb14542b17bc01e426511361
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-06-06 02:55:26 +0300

    New

--- FutureCode1603/settings.py
+++ FutureCode1603/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'FutureCode1603.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'NAME': 'futurecode1603',
+        'USER': 'postgres',
+        'PASSWORD': '1'
 
     }
 }
